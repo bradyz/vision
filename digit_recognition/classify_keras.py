5916431ecd0a3fc04963dbd8ab045219af805023
bradyz <brady.zhou@utexas.edu>
2017-02-18 14:09:49 -0600

    testing out fine tune vgg

--- digit_recognition/classify_keras.py
+++ digit_recognition/classify_keras.py
@@ -1,36 +1,36 @@
-from tensorflow.examples.tutorials.mnist import input_data
+import sys
 
-from matplotlib import pyplot as plt
+from tensorflow.examples.tutorials.mnist import input_data
 
 import numpy as np
 
-from keras.models import Sequential, Model
-from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.applications.vgg16 import VGG16
+from keras.callbacks import CSVLogger, ModelCheckpoint, ReduceLROnPlateau
 from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Dense, Dropout, Activation, Flatten, Input
+from keras.models import Sequential, Model
+
+import helpers
 
 
 SAVE_FILE = 'model_weights.h5'
+LOG_FILE = 'training.log'
 NUM_CLASSES = 10
 IMAGE_SIZE = 28
 
 
-def show(image):
-    plt.imshow(image, cmap='gray')
-    plt.show()
-    return
-
-
 def load_model(used_saved=False):
     model = Sequential()
-    model.add(Convolution2D(16, 3, 3, border_mode='valid',
+    model.add(Convolution2D(32, 10, 10, border_mode='same',
                             input_shape=(IMAGE_SIZE, IMAGE_SIZE, 1),
                             name='conv1'))
     model.add(Activation('relu', name='relu1'))
-    model.add(Convolution2D(16, 2, 2, border_mode='valid', name='conv2'))
+    model.add(Convolution2D(16, 2, 2, border_mode='same', name='conv2'))
     model.add(Activation('relu', name='relu2'))
     model.add(MaxPooling2D(pool_size=(2, 2), name='pool2'))
     model.add(Flatten(name='flatten2'))
     model.add(Dense(256, name='fc3'))
+    model.add(Dropout(0.25))
     model.add(Activation('relu', name='relu3'))
     model.add(Dense(NUM_CLASSES, name='fc4'))
     model.add(Activation('softmax', name='softmax4'))
@@ -38,27 +38,45 @@ def load_model(used_saved=False):
     if used_saved:
         model.load_weights(SAVE_FILE, by_name=True)
 
-    model.compile(loss='categorical_crossentropy',
-                  optimizer="adadelta",
-                  metrics=['accuracy'])
-
     return model
 
 
 def train(model):
     data = input_data.read_data_sets('MNIST_data/', one_hot=True)
 
-    x_train = data.train.images
-    x_train = np.reshape(x_train, (len(x_train), IMAGE_SIZE, IMAGE_SIZE, 1))
-    y_train = data.train.labels
+    model.compile(loss='categorical_crossentropy',
+                  optimizer="adadelta",
+                  metrics=['accuracy'])
 
-    model.fit(x_train, y_train, batch_size=32, nb_epoch=2, verbose=1)
+    x_train = data.train.images[:1000]
+    x_train = np.reshape(x_train, (len(x_train), IMAGE_SIZE, IMAGE_SIZE, 1))
+    x_train = helpers.convert_greyscale_to_rgb(x_train)
+    x_train = helpers.resize_dataset(x_train, 224, 224)
+
+    y_train = data.train.labels[:1000]
+
+    x_valid = data.validation.images[:100]
+    x_valid = np.reshape(x_valid, (len(x_valid), IMAGE_SIZE, IMAGE_SIZE, 1))
+    x_valid = helpers.convert_greyscale_to_rgb(x_valid)
+    x_valid = helpers.resize_dataset(x_valid, 224, 224)
+
+    y_valid = data.validation.labels[:100]
+
+    checkpointer = ModelCheckpoint(filepath=SAVE_FILE, verbose=1)
+    csv_logger = CSVLogger(LOG_FILE)
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5,
+                                  patience=2, min_lr=0.001)
+    debugger = helpers.ManualInspection(model)
+
+    model.fit(x_train, y_train, batch_size=32, nb_epoch=20, verbose=1,
+              validation_data=(x_valid, y_valid),
+              callbacks=[checkpointer, csv_logger, reduce_lr, debugger])
 
     model.save_weights(SAVE_FILE)
     return
 
 
-def test(model):
+def debug(model):
     data = input_data.read_data_sets('MNIST_data/', one_hot=True)
 
     x_sample = data.test.images[0]
@@ -70,12 +88,33 @@ def test(model):
 
     # Visualize activation layers of max pool 2.
     for i in range(16):
-        show(intermediate_output[0, :, :, i])
+        helpers.show(intermediate_output[0, :, :, i])
+
+    conv1_weights = model.get_layer('conv1').get_weights()[0]
+    helpers.view_filters(conv1_weights)
 
 
-model = load_model(True)
+def load_model_vgg():
+    img_input = Input(tensor=Input(shape=(224, 224, 3)))
 
-for layer in model.layers:
-    print(layer, layer.name)
+    base_model = VGG16(include_top=False, input_tensor=img_input)
 
-test(model)
+    # Freeze all layers in pretrained network.
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    x = base_model.output
+    x = Flatten(name='flatten')(x)
+    x = Dense(4096, activation='relu', name='fc1')(x)
+    x = Dense(4096, activation='relu', name='fc2')(x)
+    x = Dense(NUM_CLASSES, activation='softmax', name='predictions')(x)
+
+    model = Model(input=img_input, output=x) 
+
+    return model
+
+
+if __name__ == "__main__":
+    model = load_model_vgg()
+
+    train(model)
