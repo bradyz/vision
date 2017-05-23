3ec55bffdacc767856b0afbc1131e0a8c9434f2b
bradyz <brady.zhou@utexas.edu>
2017-05-22 19:19:33 -0700

    mini batch sgd

--- neural_network/backprop.py
+++ neural_network/backprop.py
@@ -59,20 +59,9 @@ def sample(function, W1, b1, W2, b2, W3, b3, num_samples=100):
     plt.pause(0.01)
 
 
-def train(function, W1, b1, W2, b2, W3, b3, n_iterations=1000000, h=1e-3):
-    for i in range(n_iterations):
-        example, label = generate(function)
-
-        activations, y = forward_pass(example, W1, b1, W2, b2, W3, b3)
-        x, y1, z1, y2, z2 = activations
-
-        # Loss is 1/2 * (y_pred - y_true)^2.
-        loss = mean_squared_error(y, label)
-
-        if np.isnan(loss):
-            print("NaN encountered at iteration %d." % i)
-            break
-        elif i % 1000 == 0:
+def train(function, W1, b1, W2, b2, W3, b3, h=1e-4, batch_size=64, n=10000000):
+    for i in range(n):
+        if i % 100 == 0:
             validation_n = 100
             validation_loss = 0.0
 
@@ -87,50 +76,74 @@ def train(function, W1, b1, W2, b2, W3, b3, n_iterations=1000000, h=1e-3):
 
             sample(function, W1, b1, W2, b2, W3, b3)
 
-        # Derivative of loss with respect to prediction.
-        dldy = np.array(y - label).reshape(1, 1)
+            continue
 
-        # Layer 3.
-        dydW3 = np.transpose(np.matrix(z2))
-        dydb3 = np.matrix(1)
+        dldW3 = np.zeros((W3.shape))
+        dldb3 = np.zeros((b3.shape))
 
-        dldW3 = np.dot(dldy, dydW3)
-        dldb3 = np.dot(dldy, dydb3)
+        dldW2 = np.zeros((W2.shape))
+        dldb2 = np.zeros((b2.shape))
 
-        check_shape(dldW3, W3)
-        check_shape(dldb3, b3)
+        dldW1 = np.zeros((W1.shape))
+        dldb1 = np.zeros((b1.shape))
 
-        # Layer 2.
-        dydz2 = np.matrix(W3)
-        dz2dy2 = dsigmoid(y2)
+        for _ in range(batch_size):
+            example, label = generate(function)
 
-        dldz2 = np.dot(np.transpose(dydz2), dldy)
-        dldy2 = np.multiply(dz2dy2, dldz2)
+            activations, y = forward_pass(example, W1, b1, W2, b2, W3, b3)
+            x, y1, z1, y2, z2 = activations
 
-        dy2dW2 = np.transpose(np.matrix(z1))
-        dy2db2 = np.matrix(1)
+            # Loss is 1/2 * (y_pred - y_true)^2.
+            loss = mean_squared_error(y, label)
 
-        dldW2 = np.dot(dldy2, dy2dW2)
-        dldb2 = np.dot(dldy2, dy2db2)
+            if np.isnan(loss):
+                print("NaN encountered at iteration %d." % i)
+                break
 
-        check_shape(dldW2, W2)
-        check_shape(dldb2, b2)
+            # Derivative of loss with respect to prediction.
+            dldy = np.array(y - label).reshape(1, 1)
 
-        # Layer 1.
-        dy2dz1 = W2
-        dz1dy1 = dsigmoid(y1)
+            # Layer 3.
+            dydW3 = np.transpose(np.matrix(z2))
+            dydb3 = np.matrix(1)
 
-        dldz1 = np.dot(np.transpose(dy2dz1), dldy2)
-        dldy1 = np.multiply(dz1dy1, dldz1)
+            dldW3 += np.dot(dldy, dydW3)
+            dldb3 += np.dot(dldy, dydb3)
 
-        dy1dW1 = x
-        dy1db1 = 1
+            check_shape(dldW3, W3)
+            check_shape(dldb3, b3)
 
-        dldW1 = np.dot(dldy1, dy1dW1)
-        dldb1 = np.dot(dldy1, dy1db1)
+            # Layer 2.
+            dydz2 = np.matrix(W3)
+            dz2dy2 = dsigmoid(y2)
 
-        check_shape(dldW1, W1)
-        check_shape(dldb1, b1)
+            dldz2 = np.dot(np.transpose(dydz2), dldy)
+            dldy2 = np.multiply(dz2dy2, dldz2)
+
+            dy2dW2 = np.transpose(np.matrix(z1))
+            dy2db2 = np.matrix(1)
+
+            dldW2 += np.dot(dldy2, dy2dW2)
+            dldb2 += np.dot(dldy2, dy2db2)
+
+            check_shape(dldW2, W2)
+            check_shape(dldb2, b2)
+
+            # Layer 1.
+            dy2dz1 = W2
+            dz1dy1 = dsigmoid(y1)
+
+            dldz1 = np.dot(np.transpose(dy2dz1), dldy2)
+            dldy1 = np.multiply(dz1dy1, dldz1)
+
+            dy1dW1 = x
+            dy1db1 = 1
+
+            dldW1 += np.dot(dldy1, dy1dW1)
+            dldb1 += np.dot(dldy1, dy1db1)
+
+            check_shape(dldW1, W1)
+            check_shape(dldb1, b1)
 
         # Apply gradients.
         W3 -= h * dldW3
@@ -144,18 +157,18 @@ def train(function, W1, b1, W2, b2, W3, b3, n_iterations=1000000, h=1e-3):
 
 
 if __name__ == "__main__":
-    n_1 = 16
-    n_2 = 16
+    n_1 = 8
+    n_2 = 8
 
-    W1 = np.random.randn(n_1, 1) * 1e-3
+    W1 = np.random.randn(n_1, 1) * 1e-2
     b1 = np.zeros((n_1, 1))
 
-    W2 = np.random.randn(n_2, n_1) * 1e-3
+    W2 = np.random.randn(n_2, n_1) * 1e-2
     b2 = np.zeros((n_2, 1))
 
-    W3 = np.random.randn(1, n_2) * 1e-3
+    W3 = np.random.randn(1, n_2) * 1e-2
     b3 = np.zeros((1, 1))
 
     plt.ion()
-    line = lambda x: np.cos(0.5 * x) + 10.0
+    line = lambda x: -3.5 * x + 10.0
     train(line, W1, b1, W2, b2, W3, b3)
