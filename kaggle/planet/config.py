9d53c0d3e8af5a930439d2296270a5bebd403c4b
Brady Zhou <bz@uber.com>
2017-07-03 23:24:11 -0700

    kaggle simpler model .7 lb

--- kaggle/planet/config.py
+++ kaggle/planet/config.py
@@ -3,9 +3,12 @@ image_dir = 'train-jpg'
 model_path = 'model_dense.h5'
 
 num_classes = 17
-batch_size = 32
+batch_size = 128
 
-image_shape = [224, 224, 3]
+image_shape = [96, 96, 3]
 
 FEATURES = ['activation_1', 'activation_10', 'activation_22',
             'activation_40', 'activation_49']
+
+image_mean = [77.09, 87.68, 80.24]
+image_std = [35.84, 37.44, 43.15]
--- kaggle/planet/model.py
+++ kaggle/planet/model.py
@@ -11,11 +11,11 @@ from keras.regularizers import l2
 from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, TensorBoard
 from keras.models import Model
 from keras.optimizers import Adam
-from keras.layers.core import Lambda, Dropout, Flatten
+from keras.layers.core import Lambda, Dropout, Flatten, Dense
 from keras.layers import Input, Conv2D
-from keras.layers.merge import Concatenate
+from keras.layers.merge import Concatenate, Add
 from keras.layers.normalization import BatchNormalization
-from keras.layers.pooling import MaxPooling2D, AveragePooling2D, GlobalAveragePooling2D
+from keras.layers.pooling import MaxPooling2D, AveragePooling2D, GlobalAveragePooling2D, GlobalMaxPooling2D
 from keras.applications import resnet50
 from keras.preprocessing.image import random_rotation
 
@@ -24,12 +24,11 @@ import helpers
 import metrics
 
 
-def weighted_loss(weighted=True, alpha=2.0, beta=1.0):
+def weighted_loss(weighted=False, alpha=2.0, beta=1.0):
     def loss(y_true, y_pred):
         left = y_true * K.log(y_pred + K.epsilon())
         right = (1.0 - y_true) * K.log(1.0 - y_pred + K.epsilon())
 
-        # Weights will penalize missing a positive more.
         if weighted:
             weights = np.log(get_class_weight(helpers.get_labels(config.train_path)))
             weights = weights / np.max(weights)
@@ -38,8 +37,8 @@ def weighted_loss(weighted=True, alpha=2.0, beta=1.0):
         else:
             weights = np.ones(config.num_classes)
 
-        log_loss = alpha * K.mean(left, axis=0) + beta * K.mean(right, axis=0)
-        log_loss = weights * log_loss
+        # Alpha will penalize missing a positive more.
+        log_loss = alpha * weights * K.mean(left, axis=0) + beta * K.mean(right, axis=0)
 
         return -1.0 * K.mean(log_loss)
 
@@ -68,12 +67,12 @@ def resize(tensor, h, w):
     return Lambda(lambda x: tf.image.resize_images(x, (h, w)))(tensor)
 
 
-def bottleneck_layer(net, k, decay=5e-4):
+def conv_block(net, k, decay=5e-5):
     net = Conv2D(k // 2, (1, 1), padding='same', kernel_regularizer=l2(decay))(net)
     net = BatchNormalization()(net)
     net = Lambda(K.relu)(net)
 
-    net = Conv2D(k, (3, 3), padding='same', kernel_regularizer=l2(decay))(net)
+    net = Conv2D(k * 2, (3, 3), padding='same', kernel_regularizer=l2(decay))(net)
     net = BatchNormalization()(net)
     net = Lambda(K.relu)(net)
 
@@ -82,17 +81,71 @@ def bottleneck_layer(net, k, decay=5e-4):
 
 def dense_block(net, k, n=3):
     for _ in range(n):
-        net = Concatenate()([net, bottleneck_layer(net, k)])
+        net = Concatenate()([net, conv_block(net, k)])
 
     return net
 
 
-def transition_block(net):
+def transition_block(net, k=32):
+    # net = BatchNormalization()(net)
+    # net = AveragePooling2D((3, 3), strides=(2, 2), padding='SAME')(net)
+    # first = Dropout(0.25)(first)
+    #
+    # second = MaxPooling2D((3, 3), strides=(2, 2), padding='SAME')(net)
+    # second = Dropout(0.25)(second)
+    #
+    # return Concatenate()([first, second])
+    net = Conv2D(k, (3, 3), strides=(2, 2), padding='same')(net)
     net = BatchNormalization()(net)
-    net = Lambda(K.sigmoid)(net)
-    net = AveragePooling2D()(net)
+    net = Lambda(K.relu)(net)
 
     return net
+
+
+def build_simple(weights_path=''):
+    input_tensor = Input(shape=config.image_shape)
+
+    # 112 x 112.
+    block1 = input_tensor
+    block1 = conv_block(block1, 64)
+    block1 = transition_block(block1, 64)
+
+    block2 = block1
+    # block2 = conv_block(block2, 16)
+    # block2 = transition_block(block2, 32)
+
+    block3 = block2
+    # block3 = conv_block(block3, 32)
+    # block3 = transition_block(block3, 64)
+
+    block4 = block3
+    block4 = conv_block(block4, 64)
+    block4 = transition_block(block4, 64)
+
+    # One activation map for each class.
+    block5 = block4
+    # block5 = conv_block(block4, 32)
+    # block5 = Dropout(0.5)(block5)
+
+    # block5 = conv_block(block5, 32)
+    # block5 = Dropout(0.5)(block5)
+
+    block5 = Conv2D(512, (1, 1), padding='same', activation='relu')(block5)
+    block5 = GlobalMaxPooling2D()(block5)
+
+    block5 = Dense(128, activation='relu')(block5)
+    block5 = Dense(config.num_classes, activation='sigmoid')(block5)
+
+    # Pack it up in a model.
+    model = Model(inputs=[input_tensor], outputs=[block5])
+    model.compile(Adam(lr=2e-4), weighted_loss(), metrics=['binary_accuracy'])
+
+    print(model.summary())
+
+    if weights_path:
+        model.load_weights(weights_path)
+
+    return model
 
 
 def build(weights_path=''):
@@ -139,13 +192,17 @@ def build(weights_path=''):
     block5 = dense_block(block5, 8)
 
     # One activation map for each class.
-    block6 = bottleneck_layer(block5, 128)
+    block6 = conv_block(block5, 128)
     block6 = Dropout(0.5)(block6)
-    block6 = bottleneck_layer(block6, 256)
+
+    block6 = conv_block(block6, 256)
     block6 = Dropout(0.5)(block6)
+
     block6 = Conv2D(config.num_classes, (1, 1), padding='same',
-                    activation='sigmoid', kernel_regularizer=l2(5e-5))(block6)
-    block6 = GlobalAveragePooling2D()(block6)
+                    kernel_regularizer=l2(5e-5))(block6)
+    block6 = GlobalMaxPooling2D()(block6)
+    block6 = BatchNormalization()(block6)
+    block6 = Lambda(K.sigmoid)(block6)
 
     # Pack it up in a model.
     model = Model(inputs=[input_tensor], outputs=[block6])
@@ -167,38 +224,35 @@ def load_image(image_path, augment=True, fileformat='%s/%s.jpg'):
 
     if augment:
         image = random_rotation(image, 180, row_axis=0, col_axis=1, channel_axis=2)
-        image += 10.0 * np.random.randn(*image.shape)
-        image = np.clip(image, 0, 255)
+        image += 5.0 * np.random.randn(*image.shape)
+    #     image = np.clip(image, 0, 255)
 
     return image
 
 
-def batch_generator(labels, batch_size, weighted=False):
+def batch_generator(labels, batch_size, weighted):
     x = np.zeros([batch_size] + config.image_shape)
     y = np.zeros([batch_size] + [config.num_classes])
 
     if weighted:
+        mapping = [list() for _ in range(config.num_classes)]
+
+        for i in range(len(labels)):
+            for j in range(config.num_classes):
+                if labels[i][1][j] == 1.0:
+                    mapping[j].append(i)
+
         # Sample inversely proportional to data frequency.
-        # class_weight = np.ones(config.num_classes)
-        class_weight = np.log(get_class_weight(labels))
+        class_weight = np.ones(config.num_classes)
+        # class_weight = np.log(get_class_weight(labels))
         # class_weight = get_class_weight(labels)
 
         class_weight = class_weight / np.sum(class_weight) * batch_size
         class_weight = np.int32(class_weight)
-
-        # Rounding errors, balance out the rest.
-        while np.sum(class_weight) < batch_size:
-            min_i = 0
-
-            # Look for least represented.
-            for i in range(class_weight.shape[0]):
-                if class_weight[i] < class_weight[min_i]:
-                    min_i = i
-
-            class_weight[min_i] += 1
+        class_weight += np.ones(config.num_classes, dtype=np.int32)
 
         # Make sure we have enough for a batch.
-        assert np.sum(class_weight) == batch_size
+        assert np.sum(class_weight) >= batch_size
 
         print(class_weight)
 
@@ -210,24 +264,24 @@ def batch_generator(labels, batch_size, weighted=False):
 
         # Load up a batch.
         for i in range(batch_size):
-            index = np.random.randint(len(labels))
-
             if weighted:
-                # Keep looking for the right class.
-                while labels[index][1][class_index] < 1.0:
-                    index = np.random.randint(len(labels))
-
-                class_counts[class_index] += 1
+                class_index = np.random.randint(config.num_classes)
 
                 # Sufficiently found enough of the class.
-                if class_counts[class_index] >= class_weight[class_index]:
-                    class_index = (class_index + 1) % class_weight.shape[0]
+                while class_counts[class_index] >= class_weight[class_index]:
+                    class_index = np.random.randint(config.num_classes)
+
+                index = mapping[class_index][np.random.randint(len(mapping[class_index]))]
+                class_counts[class_index] += 1
+            else:
+                index = np.random.randint(len(labels))
 
             x[i] = load_image(labels[index][0])
             y[i] = labels[index][1]
 
         # Batchwise preprocess images.
-        x = resnet50.preprocess_input(x)
+        # x = resnet50.preprocess_input(x)
+        # x = (x - config.image_mean)
 
         yield x, y
 
@@ -238,7 +292,7 @@ def get_datagen(train_path, batch_size, validation_split=0.2):
 
     index = round(validation_split * len(labels))
     datagen = batch_generator(labels[index:], batch_size, True)
-    valid_datagen = batch_generator(labels[:index], batch_size, False)
+    valid_datagen = batch_generator(labels[:index], batch_size, True)
 
     return datagen, valid_datagen
 
@@ -337,16 +391,15 @@ def test(model, datagen, valid_datagen):
 
 
 def train(model, datagen, valid_datagen):
+    validation_steps = 10
     train_steps = 500
-    validation_steps = 50
     num_epochs = 100
 
     callbacks = list()
     callbacks.append(ModelCheckpoint(config.model_path, verbose=1))
     callbacks.append(ReduceLROnPlateau(factor=0.5, patience=3,
                                        verbose=1, epsilon=0.01))
-    callbacks.append(metrics.MetricCallback(model, valid_datagen,
-                                            validation_steps))
+    callbacks.append(metrics.MetricCallback(model, valid_datagen))
     callbacks.append(TensorBoard(write_graph=False))
 
     model.fit_generator(datagen,
@@ -356,11 +409,28 @@ def train(model, datagen, valid_datagen):
                         callbacks=callbacks)
 
 
+def get_stats(datagen, num_batches=100):
+    mean = np.zeros(3, dtype=np.float32)
+    std = np.zeros(3, dtype=np.float32)
+    n = 0
+
+    for _ in range(num_batches):
+        x, _ = next(datagen)
+
+        n += 1
+
+        mean = (n - 1) / n * mean + 1.0 / n * np.mean(x, axis=(0,1,2))
+        std = (n - 1) / n * std + 1.0 / n * np.std(x, axis=(0,1,2))
+
+    return mean, std
+
+
 def main():
     np.random.seed(0)
     np.set_printoptions(precision=2)
 
-    model = build()
+    # model = build()
+    model = build_simple()
     datagen, valid_datagen = get_datagen(config.train_path, config.batch_size)
 
     # Train the model.
