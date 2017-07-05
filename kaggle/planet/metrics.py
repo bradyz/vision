c6b7178da1b24b042f21d13b75c0ca692b403f31
Brady Zhou <bz@uber.com>
2017-07-04 20:57:03 -0700

    batchwise f score

--- kaggle/planet/metrics.py
+++ kaggle/planet/metrics.py
@@ -20,23 +20,37 @@ def print_metrics(TP, FP, FN):
 
     average_f_score = np.mean(f_score[np.isfinite(f_score)])
 
+    support = TP + FN
+
     print()
     print('Precision:\n%s' % precision)
     print('Recall:\n%s' % recall)
+    print('Support:\n%s' % support)
     print('F_score:\n%s' % f_score)
     print('F_score:\t%s' % average_f_score)
 
 
 class MetricCallback(Callback):
-    def __init__(self, model, valid_datagen, steps):
+    def __init__(self, model, valid_datagen, steps=10, num_batches=50):
         self.model = model
         self.valid_datagen = valid_datagen
+
         self.steps = steps
+        self.num_batches = num_batches
+
+        self.clear()
+
+    def clear(self):
+        self.TP = np.zeros(config.num_classes, dtype=np.int32)
+        self.FP = np.zeros(config.num_classes, dtype=np.int32)
+        self.FN = np.zeros(config.num_classes, dtype=np.int32)
 
     def on_epoch_end(self, epoch, logs={}):
-        TP = np.zeros(config.num_classes, dtype=np.int32)
-        FP = np.zeros(config.num_classes, dtype=np.int32)
-        FN = np.zeros(config.num_classes, dtype=np.int32)
+        self.clear()
+
+    def on_batch_end(self, batch, logs={}):
+        if batch == 0 or batch % self.num_batches != 0:
+            return
 
         for i, x_y in enumerate(self.valid_datagen):
             if i == self.steps:
@@ -45,8 +59,8 @@ class MetricCallback(Callback):
             y_true = np.round(x_y[1]).astype('bool')
             y_pred = np.round(self.model.predict_on_batch(x_y[0])).astype('bool')
 
-            TP += np.sum(np.logical_and(y_pred == True, y_true == True), axis=0)
-            FP += np.sum(np.logical_and(y_pred == True, y_true == False), axis=0)
-            FN += np.sum(np.logical_and(y_pred == False, y_true == True), axis=0)
+            self.TP += np.sum(np.logical_and(y_pred == True, y_true == True), axis=0)
+            self.FP += np.sum(np.logical_and(y_pred == True, y_true == False), axis=0)
+            self.FN += np.sum(np.logical_and(y_pred == False, y_true == True), axis=0)
 
-        print_metrics(TP, FP, FN)
+        print_metrics(self.TP, self.FP, self.FN)
