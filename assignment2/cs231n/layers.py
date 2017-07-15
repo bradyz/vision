68a42dc369dc547245de00180979630dddd59c08
bradyz <brady.zhou@utexas.edu>
2017-07-15 15:15:42 -0700

    affine, relu forward backwards

--- assignment2/cs231n/layers.py
+++ assignment2/cs231n/layers.py
@@ -2,6 +2,83 @@ from builtins import range
 import numpy as np
 
 
+def affine_forward(x, w, b):
+    """
+    Computes the forward pass for an affine (fully-connected) layer.
+
+    The input x has shape (N, d_1, ..., d_k) and contains a minibatch of N
+    examples, where each example x[i] has shape (d_1, ..., d_k). We will
+    reshape each input into a vector of dimension D = d_1 * ... * d_k, and
+    then transform it to an output vector of dimension M.
+
+    Inputs:
+    - x: A numpy array containing input data, of shape (N, d_1, ..., d_k)
+    - w: A numpy array of weights, of shape (D, M)
+    - b: A numpy array of biases, of shape (M,)
+
+    Returns a tuple of:
+    - out: output, of shape (N, M)
+    - cache: (x, w, b)
+    """
+    x_reshape = np.reshape(x, (x.shape[0], -1))
+
+    return np.dot(x_reshape, w) + b, (x, w, b)
+
+
+def affine_backward(dout, cache):
+    """
+    Computes the backward pass for an affine layer.
+
+    Inputs:
+    - dout: Upstream derivative, of shape (N, M)
+    - cache: Tuple of:
+      - x: Input data, of shape (N, d_1, ... d_k)
+      - w: Weights, of shape (D, M)
+
+    Returns a tuple of:
+    - dx: Gradient with respect to x, of shape (N, d1, ..., d_k)
+    - dw: Gradient with respect to w, of shape (D, M)
+    - db: Gradient with respect to b, of shape (M,)
+    """
+    x, w, _ = cache
+    x_reshape = np.reshape(x, (x.shape[0], -1))
+
+    dx = np.reshape(np.dot(dout, w.T), x.shape)
+    dw = np.dot(x_reshape.T, dout)
+    db = np.sum(dout, axis=0)
+
+    return dx, dw, db
+
+
+def relu_forward(x):
+    """
+    Computes the forward pass for a layer of rectified linear units (ReLUs).
+
+    Input:
+    - x: Inputs, of any shape
+
+    Returns a tuple of:
+    - out: Output, of the same shape as x
+    - cache: x
+    """
+    return np.maximum(x, 0.0), x
+
+
+def relu_backward(dout, cache):
+    """
+    Computes the backward pass for a layer of rectified linear units (ReLUs).
+
+    Input:
+    - dout: Upstream derivatives, of any shape
+    - cache: Input x, of same shape as dout
+
+    Returns:
+    - dx: Gradient with respect to x
+    """
+    x = cache
+    return np.float32(x > 0.0) * dout
+
+
 def dropout_forward(x, dropout_param):
     """
     Performs the forward pass for (inverted) dropout.
