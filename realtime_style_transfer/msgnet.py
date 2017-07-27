51fd15c528163cf2e47a851317a53185637a3b04
Brady Zhou <bz@uber.com>
2017-07-26 23:58:52 -0700

    fail adain, add msgnet

--- realtime_style_transfer/msgnet.py
+++ realtime_style_transfer/msgnet.py
@@ -10,26 +10,34 @@ from PIL import Image
 import config
 
 
-def conv(x, k, c_out, post_scope=''):
+def conv(x, k, c_out, stride=1, post=''):
     c_in = x.get_shape().as_list()[-1]
 
-    x = tf.pad(x, [[0, 0], [1, 1], [1, 1], [0, 0]], mode='REFLECT')
+    # x = tf.pad(x, [[0, 0], [1, 1], [1, 1], [0, 0]], mode='REFLECT')
     std = 1.0 / tf.sqrt(1.0 * k * k * c_in)
 
-    with tf.variable_scope('conv%s' % post_scope):
+    with tf.variable_scope('conv%s' % post):
         W = tf.get_variable('W', [k, k, c_in, c_out],
                 initializer=tf.random_uniform_initializer(-std, std, seed=0))
         b = tf.get_variable('b', [c_out],
                 initializer=tf.constant_initializer(0.0))
 
-        Wx = tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='VALID')
+        Wx = tf.nn.conv2d(x, W, strides=[1, stride, stride, 1], padding='SAME')
         y = tf.nn.bias_add(Wx, b)
 
     return y
 
 
+def conv7x7(x, c_out, post=''):
+    return conv(x, 7, c_out, post='_7x7' + post)
+
+
 def conv3x3(x, c_out, post=''):
-    return conv(x, 3, c_out, post_scope='_3x3' + post)
+    return conv(x, 3, c_out, post='_3x3' + post)
+
+
+def conv1x1(x, c_out, stride=1, post=''):
+    return conv(x, 1, c_out, stride, post='_1x1' + post)
 
 
 def relu(x):
@@ -66,40 +74,35 @@ def get_activations(x_op, op_name):
     return vgg.layers
 
 
-def get_mean_std(x_op, eps=1e-12):
-    # Shape (n, c).
-    x_mean_op, x_var_op = tf.nn.moments(x_op, axes=(1, 2), keep_dims=True)
-    x_std_op = tf.sqrt(x_var_op + eps)
-
-    return x_mean_op, x_std_op
-
-
-def adaIN(c_activations_op, s_activations_op):
-    """
-    Arguments:
-        c_activations_op (tensor): of shape (n, h, w, c)
-        s_activations_op (tensor): of shape (n, h, w, c)
-
-    Returns:
-        (tensor) of shape (n, h, w, c) c aligned with s.
-    """
-    with tf.name_scope('AdaIN'):
-        c_mean_op, c_std_op = get_mean_std(c_activations_op)
-        s_mean_op, s_std_op = get_mean_std(s_activations_op)
-
-        c_normalized_op = (c_activations_op - c_mean_op) / c_std_op
-        c_shifted_op = s_std_op * c_normalized_op + s_mean_op
-
-    return c_shifted_op
-
-
-def residual_block(x, k, scope):
+def residual_block(x, k, scope='residual_down'):
     with tf.variable_scope(scope):
         x_id = x
+        x_id = conv1x1(x_id, k, 2, post='res')
 
-        x = conv3x3(x, k, '_1')
+        x = conv1x1(x, k // 4, post='_1')
         x = relu(x)
-        x = conv3x3(x, k, '_2')
+        x = conv1x1(x, k // 4, 2, post='_2')
+        x = relu(x)
+        x = conv1x1(x, k, post='_3')
+
+        x = x_id + x
+        x = relu(x)
+
+    return x
+
+
+def residual_block_up(x, k, scope='residual_up'):
+    with tf.variable_scope(scope):
+        x_id = x
+        x_id = bilinear_up(x_id)
+        x_id = conv1x1(x_id, k, post='res')
+
+        x = conv1x1(x, k // 4, post='_1')
+        x = relu(x)
+        x = conv1x1(x, k // 4, post='_2')
+        x = relu(x)
+        x = bilinear_up(x)
+        x = conv1x1(x, k, post='_3')
 
         x = x_id + x
         x = relu(x)
@@ -114,37 +117,52 @@ def bilinear_up(x):
             method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
 
 
-def get_decoder_output(t_op):
+def inspiration_layer(x, s_gram, post=''):
+    n, h, w, c = x.shape.as_list()
+
+    with tf.variable_scope('inspiration%s' % post):
+        x = tf.reshape(x, [n, h * w, c])
+
+        W = tf.get_variable('W', [c, c],
+                initializer=tf.contrib.layers.xavier_initializer())
+
+        WG = tf.map_fn(lambda gram: tf.matmul(W, gram), s_gram)
+        x = tf.matmul(x, WG)
+        x = tf.reshape(x, [n, h, w, c])
+
+    return x
+
+
+def get_decoder_output(c_op, s_gram_list):
     with tf.variable_scope('decoder'):
-        net = t_op
+        net = c_op
 
         with tf.variable_scope('block1'):
-            net = residual_block(net, 512, 'res1')
-            net = conv3x3(net, 256)
+            net = conv7x7(net, 64)
             net = relu(net)
-            net = bilinear_up(net)
+            net = inspiration_layer(net, s_gram_list[0])
+            net = residual_block(net, 128)
 
         with tf.variable_scope('block2'):
-            net = residual_block(net, 256, 'res1')
-            net = residual_block(net, 256, 'res2')
-            net = conv3x3(net, 128)
-            net = relu(net)
-            net = bilinear_up(net)
+            net = inspiration_layer(net, s_gram_list[1])
+            net = residual_block(net, 256)
 
         with tf.variable_scope('block3'):
-            net = residual_block(net, 128, 'res1')
-            net = conv3x3(net, 64)
-            net = relu(net)
-            net = bilinear_up(net)
+            net = inspiration_layer(net, s_gram_list[2])
+            net = residual_block(net, 512)
 
         with tf.variable_scope('block4'):
-            net = residual_block(net, 64, 'res1')
-            net = conv3x3(net, 3, '_2')
-            # net = 255.0 * net
-            # net = net + (103.939, 116.78, 123.68)
-            # net = net[:, :, :, ::-1]
-            # net = tf.clip_by_value(net, 0.0, 256.0)
-            net = (tf.nn.tanh(net) + 1) * 255.0 / 2.0
+            net = inspiration_layer(net, s_gram_list[3])
+
+        with tf.variable_scope('block5'):
+            net = residual_block_up(net, 256)
+
+        with tf.variable_scope('block6'):
+            net = residual_block_up(net, 128)
+
+        with tf.variable_scope('block7'):
+            net = residual_block_up(net, 64)
+            net = conv7x7(net, 3)
 
     return net
 
@@ -153,29 +171,29 @@ def mean_l2_diff(x, y):
     return tf.reduce_mean(tf.squared_difference(x, y))
 
 
-def get_loss_op(t_op, z_content_op, c_style_op_list, s_style_op_list):
+def get_loss_op(z_op, z_layer_list, z_gram_list, c_layer_list, s_gram_list):
     with tf.name_scope('loss'):
+        content_loss_op = 0.0
+
+        for z_layer_op, c_layer_op in zip(z_layer_list, c_layer_list):
+            content_loss_op += mean_l2_diff(z_layer_op, c_layer_op)
+
         style_loss_op = 0.0
 
-        for c_style_op, s_style_op in zip(c_style_op_list, s_style_op_list):
-            c_style_mean_op, c_style_std_op = get_mean_std(c_style_op)
-            s_style_mean_op, s_style_std_op = get_mean_std(s_style_op)
-
-            style_loss_op += mean_l2_diff(c_style_mean_op, s_style_mean_op)
-            style_loss_op += mean_l2_diff(c_style_std_op, s_style_std_op)
-
-        content_loss_op = mean_l2_diff(z_content_op, t_op)
-        style_loss_op *= 1e-3
+        for z_gram_op, s_gram_op in zip(z_gram_list, s_gram_list):
+            style_loss_op += mean_l2_diff(z_gram_op, s_gram_op)
 
         loss_op = style_loss_op + content_loss_op
 
     return loss_op, style_loss_op, content_loss_op
 
 
-def get_summary(c_op, s_op, z_op, loss_op, style_loss_op, content_loss_op,
-                c_content_op, s_content_op, z_content_op,
-                grad_var_op, learn_rate_op):
+def get_summary(c_op, c_layer_list, s_op, s_gram_list,
+                z_op, z_layer_list, z_gram_list, layer_names,
+                loss_op, style_loss_op, content_loss_op,
+                learn_rate_op, grad_var_op):
     tf.summary.scalar('learn_rate', learn_rate_op)
+
     tf.summary.scalar('content_loss', content_loss_op)
     tf.summary.scalar('style_loss', style_loss_op)
     tf.summary.scalar('total_loss', loss_op)
@@ -184,13 +202,16 @@ def get_summary(c_op, s_op, z_op, loss_op, style_loss_op, content_loss_op,
     tf.summary.image('style', tf.cast(s_op, tf.uint8), 10)
     tf.summary.image('generated', tf.cast(tf.clip_by_value(z_op, 0.0, 255.0), tf.uint8), 10)
 
-    c_content_op = tf.transpose(c_content_op[:1], (3, 1, 2, 0))
-    s_content_op = tf.transpose(s_content_op[:1], (3, 1, 2, 0))
-    z_content_op = tf.transpose(z_content_op[:1], (3, 1, 2, 0))
+    for i, layer_name in enumerate(layer_names):
+        c_layer_op = tf.transpose(c_layer_list[0][:1], (3, 1, 2, 0))
+        z_layer_op = tf.transpose(z_layer_list[0][:1], (3, 1, 2, 0))
 
-    tf.summary.image('c_activations', tf.cast(c_content_op, tf.float32), 30)
-    tf.summary.image('s_activations', tf.cast(s_content_op, tf.float32), 30)
-    tf.summary.image('z_activations', tf.cast(z_content_op, tf.float32), 30)
+        tf.summary.image('c_activations/' + layer_name, tf.to_float(c_layer_op), 30)
+        tf.summary.image('z_activations/' + layer_name, tf.to_float(z_layer_op), 30)
+
+    for i, layer_name in enumerate(layer_names):
+        tf.summary.image('s_gram/' + layer_name, tf.expand_dims(s_gram_list[i], axis=-1))
+        tf.summary.image('z_gram/' + layer_name, tf.expand_dims(z_gram_list[i], axis=-1))
 
     for dx_x in grad_var_op:
         dx, x = dx_x
@@ -257,34 +278,35 @@ def get_layers(activations, layer_names):
     return [op.output for op in activations if op.name in layer_names]
 
 
-def main(save_name, batch_size=1):
-    content_layers = ['block4_conv1']
-    style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1', 'block4_conv1']
+def gram_matrix(x):
+    n, h, w, c = x.shape.as_list()
+
+    u = tf.transpose(x, [0, 3, 1, 2])
+    u = tf.reshape(u, [n, c, h * w])
+
+    return tf.matmul(u, tf.transpose(u, [0, 2, 1]))
+
+
+def main(save_name, batch_size=2):
+    layer_names = ['block1_conv1', 'block2_conv1', 'block3_conv1', 'block4_conv1']
+
+    c_op = tf.placeholder(tf.float32, shape=[batch_size] + config.input_shape)
+    c_activations = get_activations(c_op, 'c')
+    c_layer_list = get_layers(c_activations, layer_names)
 
     s_op = tf.placeholder(tf.float32, shape=[batch_size] + config.input_shape)
-    c_op = tf.placeholder(tf.float32, shape=[batch_size] + config.input_shape)
-
     s_activations = get_activations(s_op, 's')
-    c_activations = get_activations(c_op, 'c')
-
-    c_content_op = get_layers(c_activations, content_layers)[0]
-    s_content_op = get_layers(s_activations, content_layers)[0]
-
-    s_style_op_list = get_layers(s_activations, style_layers)
-    c_style_op_list = get_layers(c_activations, style_layers)
-
-    t_op = adaIN(c_content_op, s_content_op)
+    s_gram_list = [gram_matrix(op) for op in get_layers(s_activations, layer_names)]
 
     # Generated.
-    z_op = get_decoder_output(t_op)
+    z_op = get_decoder_output(c_op, s_gram_list)
     z_activations = get_activations(z_op, 'z')
-    z_content_op = get_layers(z_activations, content_layers)[0]
+    z_layer_list = get_layers(z_activations, layer_names)
+    z_gram_list = [gram_matrix(op) for op in z_layer_list]
 
     # Content, style, variation.
-    loss_op, style_loss_op, content_loss_op = get_loss_op(t_op,
-                                                          z_content_op,
-                                                          c_style_op_list,
-                                                          s_style_op_list)
+    loss_op, style_loss_op, content_loss_op = get_loss_op(
+            z_op, z_layer_list, z_gram_list, c_layer_list, s_gram_list)
 
     # Used for saving and gradient descent.
     trainable_variables = get_trainable_variables()
@@ -295,24 +317,20 @@ def main(save_name, batch_size=1):
     # All things training related.
     with tf.name_scope('training'):
         step_op = tf.Variable(0, name='step', trainable=False)
-        learn_rate_op = tf.train.exponential_decay(1e-6, step_op,
+        learn_rate_op = tf.train.exponential_decay(1e-8, step_op,
                                                    100000, 0.1, staircase=True)
         optimizer_op = tf.train.AdamOptimizer(learn_rate_op)
 
         grad_var_op = optimizer_op.compute_gradients(loss_op,
                                                      var_list=trainable_variables)
-        # clipped_grad_var_op = [(tf.clip_by_value(g, -1., 1.), v) for g, v in grad_var_op]
-        # train_op = optimizer_op.apply_gradients(clipped_grad_var_op,
-        #                                         global_step=step_op)
-        train_op = optimizer_op.minimize(loss_op,
-                                         global_step=step_op,
-                                         var_list=trainable_variables)
+        train_op = optimizer_op.apply_gradients(grad_var_op,
+                                                global_step=step_op)
 
 
-    summary_op = get_summary(c_op, s_op, z_op, loss_op, style_loss_op,
-                             content_loss_op,
-                             c_content_op, s_content_op, z_content_op,
-                             grad_var_op, learn_rate_op)
+    summary_op = get_summary(c_op, c_layer_list, s_op, s_gram_list,
+                             z_op, z_layer_list, z_gram_list, layer_names,
+                             loss_op, style_loss_op, content_loss_op,
+                             learn_rate_op, grad_var_op)
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
